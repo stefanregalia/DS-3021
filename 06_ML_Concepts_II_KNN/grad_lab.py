9c5c430a7fc888145c9fd5c7c28a31e342328637
ali-rivera <wat6sv@virginia.edu>
2025-02-27 16:33:55 +0000

    Edits on grad_lab.py

--- 06_ML_Concepts_II_KNN/grad_lab.py
+++ 06_ML_Concepts_II_KNN/grad_lab.py
@@ -16,7 +16,7 @@ build a model to answer the question you created for this dataset (make sure it 
 if needed changed the target variable.
 
 3. Create a dataframe that includes the test target values, test predicted values, 
-and tes probabilities of the positive class.
+and test probabilities of the positive class.
 
 4. No code question: If you adjusted the k hyperparameter what do you think would
 happen to the threshold function? Would the confusion look the same at the same threshold 
@@ -26,8 +26,9 @@ levels or not? Why or why not?
 concerns or positive elements do you have about the model as it relates to your question? 
 
 6. Create two functions: One that cleans the data & splits into training|test and one that 
-allows you to train and test the model with different k and threshold values. Try not to use variable names in the 
-functions, but if you need to that's fine. (If you can't get the k function and threshold function to work in one
+allows you to train and test the model with different k and threshold values, then use them to 
+optimize your model (test your model with several k and threshold combinations). Try not to use variable names 
+in the functions, but if you need to that's fine. (If you can't get the k function and threshold function to work in one
 function just run them separately.) 
 
 7. How well does the model perform? Did the interaction of the adjusted thresholds and k values help the model? Why or why not? 
